3af3968636627f16d41bfdcace532114950d70ec
leedabin2 <bindalee02@gmail.com>
2024-04-19 17:16:16 +0000

    style: 주석 설명 추가

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -17,28 +17,28 @@ void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
+// port 번호를 인자로 받아서 클라이언트에 요청이 들어 올때마다 새로운 연결 소켓을 만들어서 doit 함수 호출
 int main(int argc, char **argv) {
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
+  struct sockaddr_storage clientaddr; // 클라이언트 연결 요청 후에 클라이언트 연결 소켓주소
 
-  /* Check command line args */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
+  if (argc != 2) { // 명령줄 인수의 수가 맞지 않으면
+    fprintf(stderr, "usage: %s <port>\n", argv[0]); // 프로그램이름과, 포트 번호를 지정하라는 메시
+    exit(1); // 종료 코드
   }
 
-  listenfd = Open_listenfd(argv[1]); // 듣기 소켓을 오픈
-  while (1) {
+  listenfd = Open_listenfd(argv[1]); // 듣기 소켓을 오픈 (해당 포트 번호에 해당하는)
+  while (1) { // 클라이언트한테 받은 연결 요청을 accept (무한 루프)
     clientlen = sizeof(clientaddr);
     connfd = Accept(listenfd, (SA *)&clientaddr,
                     &clientlen);  // line:netp:tiny:accept 반복적으로 연결 요청을 접수
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
+                0); // hostname, port 반환
     printf("Accepted connection from (%s, %s)\n", hostname, port);
     doit(connfd);   // line:netp:tiny:doi 트랜잭션 수행
-    Close(connfd);  // line:netp:tiny:close 자신과의 연결 끝을 닫는다.
+    Close(connfd);  // line:netp:tiny:close 자신과의 연결 끊음(서버 연결 식별자 닫음)
   }
 }
 
@@ -51,9 +51,9 @@ void doit(int fd) {
   rio_t rio;
 
   // 클라이언트가 rio로 보낸 요청라인, 헤더를 읽고 분석
-  Rio_readinitb(&rio,fd); 
-  Rio_readlineb(&rio,buf,MAXLINE); 
-  printf("Request headers: \n");
+  Rio_readinitb(&rio,fd); // connfd를 연결하여 rio에 저장
+  Rio_readlineb(&rio,buf,MAXLINE); // rio에 있는 str을 모두 버퍼에 옮김
+  printf("Request headers: \n"); 
   printf("%s",buf); // GET: gozilla.gif
   sscanf(buf,"%s %s %s", method, uri, version); 
 
@@ -67,7 +67,7 @@ void doit(int fd) {
 
   is_static = parse_uri(uri,filename,cgiargs);
 
-  if(stat(filename, &sbuf) < 0) {
+  if(stat(filename, &sbuf) < 0) { // filename 유효성 검사
     clienterror(fd,filename,"404","Not found","Tiny couldn't find this file");
     return;
   }
@@ -107,6 +107,8 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
   sprintf(buf,"Content-type: text/html\r\n");
   Rio_writen(fd,buf,strlen(buf));
   sprintf(buf,"Content-length: %d\r\n\r\n",(int)strlen(body));
+
+  // 에러 메시지와 응답을 서버 소켓을 통해 클라이언트에게 보
   Rio_writen(fd, buf, strlen(buf));
   Rio_writen(fd, body, strlen(body));  
 }
@@ -124,6 +126,7 @@ void read_requesthdrs(rio_t *rp) {
   return;
 }
 
+// uri를 받아 요청받은 filename, cgiargs를 반
 int parse_uri(char *uri, char *filename, char *cgiargs ) {
   char *ptr;
 
@@ -134,9 +137,9 @@ int parse_uri(char *uri, char *filename, char *cgiargs ) {
     if (uri[strlen(uri)-1] == '/')
       strcat(filename,"home.html");
 
-    return 1;
+    return 1; 
   }
-  else { // 동적 컨텐츠
+  else { // 동적 컨텐츠(cgi-bin은 동적 파일로 분류(과제요구사항))
     ptr = index(uri,"?");
     if (ptr) { // ?가 있으면
       strcpy(cgiargs,ptr+1); 
@@ -151,6 +154,8 @@ int parse_uri(char *uri, char *filename, char *cgiargs ) {
   } 
 }
 
+// 클라이언트가 원하는 정적 컨텐츠를 받아옴
+// 응답 라인과 헤더를 작성 후 서버에 보내고, 정적 컨텐츠 파일을 읽어 응답을 클라에 보냄
 void serve_static(int fd, char *filename, int filesize) {
   int srcfd;
 
@@ -166,13 +171,17 @@ void serve_static(int fd, char *filename, int filesize) {
   printf("Response headers: \n");
   printf("%s", buf);
 
+  // 클라에게 response body로 보내줌
   srcfd = Open(filename, O_RDONLY, 0);
+
+  // mmap 함수는 요청한 파일을 가상메모리 영역으로 매핑
   srcp = Mmap(0,filesize,PROT_READ, MAP_PRIVATE, srcfd, 0);
   Close(srcfd);
-  Rio_writen(fd,srcp,filesize);
-  Munmap(srcp,filesize);
+  Rio_writen(fd,srcp,filesize); // 주소 srcp에서 시작하는 filesize 바이트를 클라의 연결 식별자로 복
+  Munmap(srcp,filesize); 
 }
 
+// filename을 조사해서 각각 식별자에 맞는 MIME타입을 filetype에 입
 void get_filetype(char *filename, char *filetype) {
   if (strstr(filename,".html"))
     strcpy(filetype,"text/html");
@@ -186,7 +195,7 @@ void get_filetype(char *filename, char *filetype) {
     strcpy(filetype,"text/plain");
 }
 
-
+// 동적 디렉토리를 받을 때, 응답라인과 헤더를 작성하고 서버에 보냄 
 void serve_dynamic(int fd, char *filename, char *cgiargs) {
   char buf[MAXLINE], *emptylist[] = {NULL};
 
@@ -196,14 +205,16 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
   sprintf(buf, "Server: Tiny Web Server\r\n");
   Rio_writen(fd, buf, strlen(buf));
 
-  if (Fork() == 0) { /* Child */
+  if (Fork() == 0) { // 자식 프로세스를 생성
   /* Real server would set all CGI vars here */
-  setenv("QUERY_STRING", cgiargs, 1);
+  setenv("QUERY_STRING", cgiargs, 1);// execve를 호출하기 전 자식프로세스는 cgi 환경변수 쿼리스트링을 설정하고 
   // method를 cgi-bin/adder.c에 넘겨주기 위해 환경변수 set
-  // setenv("REQUEST_METHOD", method, 1);
-  Dup2(fd, STDOUT_FILENO); /* Redirect stdout to client */
-  Execve(filename, emptylist, environ); /* Run CGI program */
-  }
-  Wait(NULL); /* Parent waits for and reaps child */
+  //setenv("REQUEST_METHOD", method, 1);
+  Dup2(fd, STDOUT_FILENO); // CGI 프로세스 출력을 fd로 복사, 실행 후 STDOUT 의 값은 fd이다. 클라이언트와 연계된 연결 식별자로 재지정
+  Execve(filename, emptylist, environ); // 파일 이름이 첫번째 인자인 것과 같은 파일을 실행
+  // /cgi-bin/adder 프로그램을 자식의 컨텍스트에서 실행
+  } 
+  Wait(NULL); // 신호를 보내면 부모의 wait이 끝남
+
   
 }
