186b1ae94d22e88a3f6fcd8a30f69583adc63a3f
leedabin2 <bindalee02@gmail.com>
2024-04-21 04:48:34 +0000

    style : adder 주석 설명 추가

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,19 +4,20 @@
 /* $begin adder */
 #include "csapp.h"
 
+// 두 숫자를 더 하는 웹서비스
 int main(void) 
 {
-  char *buf, *p, *method;
-  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  char *buf, *p, *method; // 쿼리스트링을 저장하기 위한 포인터, 문자열 내 특정위치를 가리키는 포인터, HTTP 메소드를 저장하기 위한 포인터
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE]; // 쿼리스트링에서 파싱한 두 인자 저장, content : HTTP 응답 본문을 구성하기 위한 문자열
   int n1=0, n2=0;
 
   /* Extract the two arguments */
   if ((buf = getenv("QUERY_STRING")) != NULL) {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p+1);
-    n1 = atoi(arg1);
+    p = strchr(buf, '&'); // 쿼리스트링을 읽어옴 &로 구분받아서 문자의 위치를 찾음
+    *p = '\0'; // 널문자로 변경 후 첫 번째 인자를 종료 (buf 문자열을 두 부분으로 나눔)
+    strcpy(arg1, buf); // 첫 번째 인자를 arg1에 복사
+    strcpy(arg2, p+1); // 두 번째 인자의 시작점을 arg2에 복사
+    n1 = atoi(arg1); // 정수로 변환
     n2 = atoi(arg2);
   }
   /* Make the response body */
